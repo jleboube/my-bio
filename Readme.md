dcead9f3d9b94ba5f5b26e06c9ce1e5377c5ca8a
Muscl3N3rd <joeleboube@gmail.com>
2025-05-28 17:32:38 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,14 @@
+
+# User Profile Web App
+
+## Requirements
+- Docker
+
+## Setup
+1. Clone the repository
+2. Run `docker-compose up --build`
+3. Visit the frontend at `http://localhost`
+4. Backend API is at `http://localhost:3001`
+
+## Development
+To develop frontend or backend individually, use standard Node.js tools inside the respective directories.
---
+++ app.jsx
@@ -0,0 +1,22 @@
+// File: app.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import Admin from "./pages/Admin";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
